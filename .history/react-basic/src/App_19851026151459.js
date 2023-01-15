ed7687f530d6992b24a7c72dded7221f7f359267
phucbao2k <baophucta2k@gmail.com>
2023-01-15 23:40:40 +0700

    d

--- .history/react-basic/src/App_19851026151459.js
+++ .history/react-basic/src/App_19851026151459.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Since 2023...
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
