b8a1a5bc0b08df2dfa6a891d44d8652701db11b9
phucbao2k <baophucta2k@gmail.com>
2023-01-19 22:15:54 +0700

    c

--- .history/react-basic/src/App_20230119221525.js
+++ .history/react-basic/src/App_20230119221525.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
-import { useState } from 'react';
-const App = () => {
-  let [year, setYear] = useState('2023');
+import {useState} from 'react';
+const App = ()=> {
+  let[year,setYear] = useState('2023');
   //đây là state trong react-hooks, phần tử đầu tiên là biến, phần tử thứ 2 là function
   //xử lý biến khi dữ liệu thay đổi
   const handleEventClick = (event) => {
-    setYear('2019');
+setYear('')
     console.log('clicked', event.target.value);
     //*event.target.value tức là lấy value từ event trong html/reactjs, thường áp dụng cho các thẻ select, input...
   }
   return (
     <div className="App">
-      <Nav />
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Since {year}...
+        Since {year}...
         </p>
-        <input type="text" value="haha" onClick={(event) => handleEventClick(event)} />
-
+        <input type="text" value="haha" onClick={(event) => handleEventClick(event)}/>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
